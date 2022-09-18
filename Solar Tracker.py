7ac9c6687d6b08ff7dead04fd435828152a34e41
Ruicheng Rao <106492499+ruichengrao@users.noreply.github.com>
2022-09-18 19:46:30 -0400

    Update Solar Tracker.py

--- Solar Tracker.py
+++ Solar Tracker.py
@@ -1,5 +1,5 @@
 import xlsxwriter as xl
-from datetime import datetimehttps://github.com/ruichengrao/ruichengrao/blob/main/Solar%20Tracker.py
+from datetime import datetime
 import schedule, time
 wb = xl.Workbook('solarPanel.xlsx')
 ws = wb.add_worksheet('A Test Sheet')
@@ -13,24 +13,33 @@ from geopy.geocoders import Nominatim
 
 
 
-#ws.write(Row, Column, )
-ws.write(0, 0, "Date")
-ws.write(0, 1, "Latitude")
-ws.write(0, 2, "Longitude")
-ws.write(0, 3, "Elevation")
-ws.write(0, 4, "Altitude")
-ws.write(0, 5, "Azimuth")
-
-print("Row 1 Added")
-wb.close()
-
 i = 0
 
+def addRow():
+    global i
+    geolocator = Nominatim(user_agent="Solar Tracker")
+    location = geolocator.geocode("201 Walt Banks Rd, Peachtree City, GA 30269")
+    lat = location.latitude
+    lon = location.longitude
+    elv = altitudo(lat=lat, lon=lon)
+    date = datetime.now()
+    tz = -4
+    alt, azm = sunpos(date, lat, lon, tz, dst=True)
+    print("Sun Position Acheived")
 
+    ws.write(i, 0, datetime.now())
+    ws.write(i, 1, lat)
+    ws.write(i, 2, lon)
+    ws.write(i, 3, elv)
+    ws.write(i, 4, alt)
+    ws.write(i, 5, azm)
+    i += 1
+    wb.close()
+    print("WorkBook Closed")
 
 schedule.every(1).seconds.do(addRow)
 print('run once')
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(10)
