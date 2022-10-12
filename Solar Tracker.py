2783a3825fcb8ec03b1395941503f15c916f1a68
Ruicheng Rao <106492499+ruichengrao@users.noreply.github.com>
2022-10-12 17:05:29 -0400

    added comments

--- Solar Tracker.py
+++ Solar Tracker.py
@@ -1,3 +1,5 @@
+#import package/modules
+
 import csv
 from pyephem_sunpath.sunpath import sunpos
 from datetime import datetime
@@ -5,22 +7,30 @@ from altitudo import altitudo
 from geopy.geocoders import Nominatim
 import schedule
 
+#csv header/stated file
 header = ['Date', 'Latitude', 'Longitude','Elvation','Altitude',"Azimuth"]
 filename = 'Solar_Tracking.csv'
 a = True
 
+#job function will run after every set time
 def job():
 
-    #getting lon/lat
+    #getting lon/lat 
     geolocator = Nominatim(user_agent="Solar Tracker")
     location = geolocator.geocode("201 Walt Banks Rd, Peachtree City, GA 30269")
-    exact_date = datetime.now()
     global lat
     lat = location.latitude
     global lon
     lon = location.longitude
+
+    #getting date
+    exact_date = datetime.now()
+
+    #getting elvation
     global elv
     elv = altitudo(lat=lat, lon=lon)
+
+    #getting azimuth
     tz = -4
     global rounded_alt
     global rounded_azm
@@ -28,11 +38,13 @@ def job():
     rounded_alt = round(alt,5)
     rounded_azm = round(azm,5)
     print("azimuth calcuated")
+    #csv data
     global data
     data = [str(exact_date),str(lat),str(lon),str(elv),str(rounded_alt) + ("°"),str(rounded_azm) + ("°")]
     global a
-    with open(filename, 'a', newline="\n" ) as file:
+    with open(filename, 'a', newline="\n") as file:
         csvwriter = csv.writer(file)
+        #skips header line after the first run
         if a:
           csvwriter.writerow(header)
           a = False
@@ -40,9 +52,9 @@ def job():
 
 job()
 
-   
+#call every x min/sec
 
-schedule.every(10).minutes.do(job)
+schedule.every(10).minute.do(job)
 
 
 while True:
