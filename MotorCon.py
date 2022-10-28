eb6d3a44f18913a140b75f290981e641ef65e347
Ruicheng Rao <106492499+ruichengrao@users.noreply.github.com>
2022-10-28 08:01:33 -0700

    Update MotorCon.py

--- MotorCon.py
+++ MotorCon.py
@@ -1,11 +1,73 @@
 from RpiMotorLib import RpiMotorLib
 import RPi.GPIO as GPIO
-import time
+import schedule
+from time import sleep, time
+from pyephem_sunpath.sunpath import sunpos
+from datetime import datetime
+from geopy.geocoders import Nominatim
+from datetime import date
+import csv
+
+
+#getting lon/lat 
+geolocator = Nominatim(user_agent="Solar Tracker")
+location = geolocator.geocode("201 Walt Banks Rd, Peachtree City, GA 30269")
+global lat
+lat = location.latitude
+global lon
+lon = location.longitude
+
+#getting date
+exact_date = datetime.now()
+
+#getting azimuth
+tz = -4
+global rounded_alt
+global rounded_azm
+alt, azm = sunpos(exact_date, lat, lon, tz, dst=False)
+rounded_alt = round(alt,5)
+rounded_azm = round(azm,5)
+
+# csv file name
+filename = "Solar_Tracking.csv"
+#currentRow = 
+# initializing the titles and rows list
+fields = []
+rows = []
+ 
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+index2 = csvreader.line_num - 2
+index1 = csvreader.line_num - 3
+
+ 
+for row in rows[index1:index2]:
+    # parsing each column of a row
+    difference = rounded_azm - float(row)
+    angle_diff = round(difference, 3)
+    
+def runner():
+    # Calculating step count
+    step_count= angle_diff / 5.625
+    global round_step
+    round_step = round(step_count, 0)
+    print(round_step)
+    
+
 
 GpioPins = [18, 23, 24, 25]
 
-# Declare an named instance of class pass a name and motor type
+
 mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
 
-# call the function , pass the parameters (GPIOPins, wait(sec), steps, ccw = False/cw = True, half = half-stepping, initDelay
-mymotortest.motor_run(GpioPins , .01, 100, False, False, "half", .05)
+
+mymotortest.motor_run(GpioPins, .01, round_step, False, True, "half", .05)
